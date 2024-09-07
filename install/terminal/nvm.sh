77a9448a59e534f6821b307c97dd5ec03bb5a58b
Chase Clark <chase_clark07@hotmail.com>
2024-09-07 15:13:05 -0400

    fix nvm

--- install/terminal/nvm.sh
+++ install/terminal/nvm.sh
@@ -5,5 +5,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 # need to use nvm command without restarting shell
 source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 
 nvm install --lts
