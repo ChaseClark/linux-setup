4fb1a4de7459976213bd1019a8000f16f67dc056
Chase Clark <chase_clark07@hotmail.com>
2024-09-07 16:26:02 -0400

    basic desktop setup

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -1,8 +1,16 @@
-# Center new windows in the middle of the screen
-gsettings set org.gnome.mutter center-new-windows true
+# fonts
+gsettings set org.gnome.desktop.interface font-name 'Inter Regular 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Inter Regular 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Cascadia Mono PL Roman 13'
 
-# Set Cascadia Mono as the default monospace font
-gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+# show weekday on top bar
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# enable maximize button on pop os
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+
+# Center new windows in the middle of the screen
+# gsettings set org.gnome.mutter center-new-windows true
 
 # Reveal week numbers in the Gnome calendar
-gsettings set org.gnome.desktop.calendar show-weekdate true
+# gsettings set org.gnome.desktop.calendar show-weekdate true
