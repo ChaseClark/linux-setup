e29d0c620001423002e90e4349f176fe831d7ec2
Chase Clark <chase_clark07@hotmail.com>
2024-09-08 15:11:56 -0400

    change boxes to flatpak

--- install/terminal/personal-apps.sh
+++ install/terminal/personal-apps.sh
@@ -1,6 +1,6 @@
 sudo apt install -y \
  git timeshift ubuntu-restricted-extras code ffmpeg \
  gcc make perl curl wget libheif1 fonts-firacode heif-gdk-pixbuf heif-thumbnailer fonts-cascadia-code \
- steam fonts-inter-variable gnome-boxes breeze-cursor-theme gir1.2-gtop-2.0 solaar libwebkit2gtk-4.1-dev \
+ steam fonts-inter-variable gir1.2-gtop-2.0 solaar libwebkit2gtk-4.1-dev \
  build-essential file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev \
  bashtop ripgrep tldr fzf
