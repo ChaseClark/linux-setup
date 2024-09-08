0666b1bdd352ea0102eceee39f76900f50701fd1
Chase Clark <chase_clark07@hotmail.com>
2024-09-08 14:48:47 -0400

    remove gnome

--- install/desktop.sh
+++ install/desktop.sh
@@ -1,13 +1,7 @@
-# Ensure computer doesn't go to sleep or lock while installing
-gsettings set org.gnome.desktop.screensaver lock-enabled false
-gsettings set org.gnome.desktop.session idle-delay 0
+# TODO: Ensure computer doesn't go to sleep or lock while installing
 
 # Run desktop installers
 for installer in ~/.local/share/pop-setup/install/desktop/*.sh; do source $installer; done
-
-# Revert to normal idle and lock settings
-gsettings set org.gnome.desktop.screensaver lock-enabled true
-gsettings set org.gnome.desktop.session idle-delay 300
 
 # final system update to update vscode mainly
 echo "updating system again..."
--- install/terminal/personal-apps.sh
+++ install/terminal/personal-apps.sh
@@ -1,5 +1,5 @@
 sudo apt install -y \
- git timeshift gnome-tweaks chrome-gnome-shell ubuntu-restricted-extras code ffmpeg \
+ git timeshift ubuntu-restricted-extras code ffmpeg \
  gcc make perl curl wget libheif1 fonts-firacode heif-gdk-pixbuf heif-thumbnailer fonts-cascadia-code \
  steam fonts-inter-variable gnome-boxes breeze-cursor-theme gir1.2-gtop-2.0 solaar libwebkit2gtk-4.1-dev \
  build-essential file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev \
