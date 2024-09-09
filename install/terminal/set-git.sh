4e629e9663e84a53edb4db0d54f92ab0d8ca5a63
Chase Clark <chase_clark07@hotmail.com>
2024-09-09 12:31:10 -0400

    Update set-git.sh

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -9,4 +9,5 @@ git config --global alias.ci commit
 git config --global alias.st status
 git config --global pull.rebase true
 
-
+# auto setup remote branches
+git config --global push.autoSetupRemote true
