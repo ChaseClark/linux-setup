f76e61e784eeb25813c0a806c5db3c19bba8145d
Chase Clark <chase_clark07@hotmail.com>
2024-09-14 15:48:54 -0400

    checkout pop24-04

--- boot.sh
+++ boot.sh
@@ -12,5 +12,10 @@ echo "Cloning linux-setup..."
 rm -rf ~/.local/share/linux-setup 
 git clone https://github.com/ChaseClark/linux-setup.git ~/.local/share/linux-setup  >/dev/null
 
+# switch to 24-04
+cd ~/.local/share/linux-setup
+git checkout pop24-04
+cd -
+
 echo "Installation starting..."
 source ~/.local/share/linux-setup/install.sh
