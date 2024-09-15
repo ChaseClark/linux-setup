3b056b1b1d03dacac0a614e46650c8473653bc60
Chase Clark <chase_clark07@hotmail.com>
2024-09-14 20:23:58 -0400

    wip

---
+++ install/desktop/set-gnome-settings.sh
@@ -0,0 +1,14 @@
+# Center new windows in the middle of the screen
+gsettings set org.gnome.mutter center-new-windows true
+
+# Set Cascadia Mono as the default monospace font
+gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+
+# Reveal week numbers in the Gnome calendar
+gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# make fonts bigger
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+
+# cursor size
+gsettings set org.gnome.desktop.interface cursor-size 16
--- install/terminal/personal-apps.sh
+++ install/terminal/personal-apps.sh
@@ -1,5 +1,5 @@
 sudo apt install -y \
- git timeshift ubuntu-restricted-extras code ffmpeg \
+ git timeshift ubuntu-restricted-extras ffmpeg \
  gcc make perl curl wget libheif1 fonts-firacode heif-gdk-pixbuf heif-thumbnailer fonts-cascadia-code \
  steam fonts-inter-variable gir1.2-gtop-2.0 solaar libwebkit2gtk-4.1-dev \
  build-essential file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev \
